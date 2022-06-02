a4abcc16ac9781079e8261961a288676eed4fde0
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-06-02 03:45:41 +0300

    Update start.py

--- program/start.py
+++ program/start.py
@@ -77,7 +77,7 @@ async def _human_time_duration(seconds):
 
 
 @Client.on_message(
-    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
+    command(["/start", f"/start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
 )
 @check_blacklist()
 async def start_(c: Client, message: Message):
